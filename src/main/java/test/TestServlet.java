ccd80548de7c3219d44749d780c8280285ae87bb
Ian <kefuyong2975@gmail.com>
2023-04-20 11:52:51 +0800

    update

--- src/main/java/test/TestServlet.java
+++ src/main/java/test/TestServlet.java
@@ -13,6 +13,6 @@ public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	response.getWriter().append("just a testing!");
+	response.getWriter().append("just a testing!!");
 	}
 }
